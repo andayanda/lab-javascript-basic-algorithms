5547a49ff5d97b5ceac9552f5f46337f08e56132
andayanda <carmelaespinola@gmail.com>
2022-07-05 22:08:42 +0200

    primera parte

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 // Iteration 1: Names and Input
 console.log("I'm ready!");//
+let hacker1;
+hacker1= "manuel";
+console.log("The driver's name is ",hacker1);
+let hacker2= "Pepe";
+console.log("The navigator's name is ",hacker2);
+
+
 // Iteration 2: Conditionals
 
 
